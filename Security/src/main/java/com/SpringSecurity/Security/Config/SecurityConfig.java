315586348c9a59d482e41570769d38a801055fc1
vamshi <vamsimangalagiri34@gmail.com>
2025-12-03 22:03:32 +0530

    inmemory

--- Security/src/main/java/com/SpringSecurity/Security/Config/SecurityConfig.java
+++ Security/src/main/java/com/SpringSecurity/Security/Config/SecurityConfig.java
@@ -2,8 +2,14 @@ package com.SpringSecurity.Security.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,14 +20,31 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable()) // Disable CSRF for simplicity in this example
-            .authorizeHttpRequests(authorize -> authorize
-                .anyRequest().permitAll() // Allow all requests for now
+            .authorizeHttpRequests(request-> request.anyRequest().authenticated()
             )
-            .formLogin(form -> form
-                .loginPage("/login") // Specify custom login page
-                .permitAll()
-            );
+//                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults()) //allows postman for login
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user= User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("1234")
+                .roles("user")
+                .build();
+
+        UserDetails admin= User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("1234")
+                .roles("role")
+                .build();
+
+        return new InMemoryUserDetailsManager(user,admin);
+    }
+
+
 }
 
